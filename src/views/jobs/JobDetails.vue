37f26d94914c1a2c129838fa3d798338ced206d8
femmytedrey <femmytedrey@gmail.com>
2024-06-17 15:52:56 +0100

    fetch job details from API

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,43 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>{{ job.description }}</p>
+  </div>
+  <div v-else>Loading job details...</div>
+  <div>{{ errorMsg }}</div>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-    // data(){
-    //     return{
-    //         id: this.$route.params.id
-    //     }
-    // }
+  props: ["id"],
+  // data(){
+  //     return{
+  //         id: this.$route.params.id
+  //     }
+  // },
+  data() {
+    return {
+      job: null,
+      errorMsg: "",
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("This is a bad request");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        this.job = data;
+        this.errorMsg = "";
+      })
+      .catch((error) => {
+        this.errorMsg = "Error:" + error.message;
+        this.job = null
+      });
+  },
 };
 </script>
 
--- src/views/jobs/JobView.vue
+++ src/views/jobs/JobView.vue
@@ -1,58 +1,59 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
   </div>
+  <div v-else>Loading jobs...</div>
+  <h4 v-if="errorMsg" class="error">{{ errorMsg }}</h4>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      jobs: [
-        {
-          id: 1,
-          title: "Frontend Developer",
-          description: "Figma or Adobe XD specialist",
-        },
-        {
-          id: 2,
-          title: "UI/UX Designer",
-          description: "React JS and Vue JS developer",
-        },
-        {
-          id: 3,
-          title: "Backend Developer",
-          description: "Node JS and Mongo DB developer",
-        },
-        {
-          id: 4,
-          title: "Data Analyst",
-          description: "Python",
-        },
-      ],
+      jobs: [],
+      errorMsg: "",
     };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
+      .then((data) => (this.jobs = data))
+      .catch((error) => {
+        this.errorMsg = "Error fetching data: " + error.message;
+      });
   },
 };
 </script>
 
 <style>
-.job h2{
-    background: #f4f4f4;
-    padding: 20px;
-    border-radius: 10px;
-    margin: 10px auto;
-    max-width: 600px;
-    cursor: pointer;
-    color: #444;
-    transition: all 0.3s ease-in-out;
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+  transition: all 0.3s ease-in-out;
 }
-.job h2:hover{
-    background: #ddd;
+.job h2:hover {
+  background: #ddd;
 }
 .job a {
-    text-decoration: none;
+  text-decoration: none;
+}
+.error {
+  color: crimson;
+  font-weight: bold;
 }
 </style>
